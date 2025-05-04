9dce618991b1bbcf8f7522a39b308b3b5af3fcf0
Icaro2M <icaromolina433@gmail.com>
2025-05-04 13:20:04 -0300

    gps atualizado

--- minimap.gd
+++ minimap.gd
@@ -2,33 +2,57 @@ extends ColorRect
 
 
 @export var target : NodePath
-@export var camera_distance := 20.0
-#@export var destino : NodePath
-#@onready var ponto_destino := get_node(destino)
+
+
 @onready var player := get_node(target)
-@onready var camera := $SubViewportContainer/SubViewport/Camera3D
+
 @onready var img := $SubViewportContainer/SubViewport/Rotas/Sprite2D
 @onready var line := $SubViewportContainer/SubViewport/Rotas/Sprite2D/Line2D
 @onready var arrow := $SubViewportContainer/SubViewport/Sprite2D2
 
 @onready var parent := $"../../vertices"
 
+@onready var listaCaminho := [$"../../vertices/Inter/Vertice14",
+							$"../../vertices/Inter/Vertice10",
+							$"../../vertices/Inter/Vertice9",			
+							$"../../vertices/Inter/Vertice6"]
+
+@onready var nodeAtual = null
+
 func _ready() -> void:
+	montar_caminho(listaCaminho,listaCaminho[0])
+		
+
+func montar_caminho(lista,node):
+	line.clear_points()
+	var idx = 0
+	if node in lista:
+		
+		for i in lista.size():
+			if lista[i]!=node:
+				idx+=1
+			else:
+				break
+	if idx==lista.size():
+		idx-=1
+
+	var listaC = lista.slice(idx,lista.size())
+	
+	print(listaC,"\n")
+	
 	var first = true
-	for child in parent.get_children():
-		for vertice in child.get_children():
-			if first:
-				line.add_point(Vector2(vertice.position.z*3+60,-vertice.position.x*3+70))
-				first = false
+	for vertice in listaC:
+		if first:
 			line.add_point(Vector2(vertice.position.z*3+60,-vertice.position.x*3+70))
-		break
+			first = false
+		line.add_point(Vector2(vertice.position.z*3+60,-vertice.position.x*3+70))
+
 
 var destino = Vector3(10, 0, 15)
 
 func _process(delta: float) -> void:
 	if target:
-		camera.size = camera_distance
-		camera.position = Vector3(player.position.x,camera_distance,player.position.z)
+		
 		img.position = Vector2(-3*player.position.z + 40,3*player.position.x+30)
 		arrow.rotation = -player.global_rotation.y + 80.1
 		
@@ -37,11 +61,23 @@ func _process(delta: float) -> void:
 		
 		line.set_point_position(0,Vector2(player.position.z*3+60,-player.position.x*3+70))
 		
-	#var origem = player.global_transform.origin
-	#var destino = ponto_destino.global_transform.origin
+		var prox = get_mais_proximo()
+		if prox!=nodeAtual:
+			nodeAtual = prox
+			montar_caminho(listaCaminho,prox)
+
+
+func get_mais_proximo():
+	var menor_dist = INF
+	var prox: Node3D = null
+	for ponto in listaCaminho:
+		var distancia = player.global_position.distance_to(ponto.global_position)
+		if distancia < menor_dist:
+			menor_dist = distancia
+			prox = ponto
 	
-	#var origem_2d = camera.unproject_position(origem)
-	#var destino_2d = camera.unproject_position(destino)
+	
+	return prox
 
 
 	
