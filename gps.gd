5e1b1eab179586727cba30f9db3b4a252f635336
Icaro2M <icaromolina433@gmail.com>
2025-05-23 16:25:51 -0300

    Dijkstra e vértices corrigidos

--- gps.gd
+++ gps.gd
@@ -1,13 +1,1 @@
 extends Node2D
-
-
-var origem_2d = Vector2()
-var destino_2d = Vector2()
-
-func _draw():
-	draw_line(origem_2d, destino_2d, Color.YELLOW, 2.0)
-
-func update_rota(origem: Vector2, destino: Vector2):
-	origem_2d = origem
-	destino_2d = destino
-	#date()  # força redesenhar
