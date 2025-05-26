e5ea355dadf854e5b8e6210c24b02d6f13327eb4
Saulo <saulobenicio2016@gmail.com>
2025-05-26 10:21:31 -0300

    adicionado 3 pontos random para passageiros

--- minimap.gd
+++ minimap.gd
@@ -1,354 +1,166 @@
 extends ColorRect
 
-enum sentidos{NORTH, SOUTH, EAST, WEST}
-
-
 @export var target : NodePath
+@export var zoom_factor : float = 1
 @onready var player := get_node(target)
 
 @onready var mapa = $"../../CanvasLayer2/mapagps"
 @onready var img := $SubViewportContainer/SubViewport/Rotas/Sprite2D
 @onready var line := $SubViewportContainer/SubViewport/Rotas/Sprite2D/Line2D
 @onready var arrow := $SubViewportContainer/SubViewport/Sprite2D2
-@onready var parent := $"../../Vtxs"
-@onready var distanceLabel = $"../distanceLabel"
+@onready var parent := $"../../vertices"
+@onready var camera := $SubViewportContainer/SubViewport/Camera3D
 
-@onready var listaCaminho:= {
-	$"../../Vtxs/Vtx":{
-		"N":null,
-		"S":$"../../Vtxs/Vtx3",
-		"E":null,
-		"W":$"../../Vtxs/Vtx2"	
-	},
-	$"../../Vtxs/Vtx2":{
-		"N":null,
-		"S":$"../../Vtxs/Vtx4",
-		"E":$"../../Vtxs/Vtx",
-		"W":null	
-	},
-	$"../../Vtxs/Vtx3":{
-		"N":$"../../Vtxs/Vtx",
-		"S":$"../../Vtxs/Vtx6",
-		"E":null,
-		"W":$"../../Vtxs/Vtx4"	
-	},
-	$"../../Vtxs/Vtx4":{
-		"N":$"../../Vtxs/Vtx2",
-		"S":null,
-		"E":$"../../Vtxs/Vtx3",
-		"W":$"../../Vtxs/Vtx5"	
-	},
-	$"../../Vtxs/Vtx5":{
-		"N":null,
-		"S":$"../../Vtxs/Vtx8",
-		"E":$"../../Vtxs/Vtx4",
-		"W":null	
-	},
-	$"../../Vtxs/Vtx6":{
-		"N":$"../../Vtxs/Vtx3",
-		"S":$"../../Vtxs/Vtx9",
-		"E":null,
-		"W":$"../../Vtxs/Vtx7"	
-	},
-	$"../../Vtxs/Vtx7":{
-		"N":null,
-		"S":$"../../Vtxs/Vtx10",
-		"E":$"../../Vtxs/Vtx6",
-		"W":$"../../Vtxs/Vtx8"	
-	},
-	$"../../Vtxs/Vtx8":{
-		"N":$"../../Vtxs/Vtx5",
-		"S":$"../../Vtxs/Vtx11",
-		"E":$"../../Vtxs/Vtx7",
-		"W":null	
-	},
-	$"../../Vtxs/Vtx9":{
-		"N":$"../../Vtxs/Vtx6",
-		"S":$"../../Vtxs/Vtx13",
-		"E":null,
-		"W":$"../../Vtxs/Vtx10"	
-	},
-	$"../../Vtxs/Vtx10":{
-		"N":$"../../Vtxs/Vtx7",
-		"S":$"../../Vtxs/Vtx14",
-		"E":$"../../Vtxs/Vtx9",
-		"W":$"../../Vtxs/Vtx11"	
-	},
-	$"../../Vtxs/Vtx11":{
-		"N":$"../../Vtxs/Vtx8",
-		"S":$"../../Vtxs/Vtx15",
-		"E":$"../../Vtxs/Vtx10",
-		"W":$"../../Vtxs/Vtx12"	
-	},
-	$"../../Vtxs/Vtx12":{
-		"N":null,
-		"S":$"../../Vtxs/Vtx16",
-		"E":$"../../Vtxs/Vtx11",
-		"W":null	
-	},
-	$"../../Vtxs/Vtx13":{
-		"N":$"../../Vtxs/Vtx9",
-		"S":$"../../Vtxs/Vtx17",
-		"E":null,
-		"W":$"../../Vtxs/Vtx14"
-	},
-	$"../../Vtxs/Vtx14":{
-		"N":$"../../Vtxs/Vtx10",
-		"S":$"../../Vtxs/Vtx18",
-		"E":$"../../Vtxs/Vtx13",
-		"W":$"../../Vtxs/Vtx15"	
-	},
-	$"../../Vtxs/Vtx15":{
-		"N":$"../../Vtxs/Vtx11",
-		"S":$"../../Vtxs/Vtx19",
-		"E":$"../../Vtxs/Vtx14",
-		"W":$"../../Vtxs/Vtx24"	
-	},
-	$"../../Vtxs/Vtx16":{
-		"N":$"../../Vtxs/Vtx12",
-		"S":null,
-		"E":$"../../Vtxs/Vtx24",
-		"W":null	
-	},
-	$"../../Vtxs/Vtx17":{
-		"N":$"../../Vtxs/Vtx13",
-		"S":$"../../Vtxs/Vtx21",
-		"E":null,
-		"W":$"../../Vtxs/Vtx18"	
-	},
-	$"../../Vtxs/Vtx18":{
-		"N":$"../../Vtxs/Vtx14",
-		"S":$"../../Vtxs/Vtx22",
-		"E":$"../../Vtxs/Vtx17",
-		"W":$"../../Vtxs/Vtx19"	
-	},
-	$"../../Vtxs/Vtx19":{
-		"N":$"../../Vtxs/Vtx15",
-		"S":$"../../Vtxs/Vtx23",
-		"E":$"../../Vtxs/Vtx18",
-		"W":$"../../Vtxs/Vtx20"	
-	},
-	$"../../Vtxs/Vtx20":{
-		"N":$"../../Vtxs/Vtx24",
-		"S":null,
-		"E":$"../../Vtxs/Vtx19",
-		"W":null	
-	},
-	$"../../Vtxs/Vtx21":{
-		"N":$"../../Vtxs/Vtx17",
-		"S":null,
-		"E":null,
-		"W":$"../../Vtxs/Vtx22"	
-	},
-	$"../../Vtxs/Vtx22":{
-		"N":$"../../Vtxs/Vtx18",
-		"S":null,
-		"E":$"../../Vtxs/Vtx21",
-		"W":$"../../Vtxs/Vtx23"	
-	},
-	$"../../Vtxs/Vtx23":{
-		"N":$"../../Vtxs/Vtx19",
-		"S":null,
-		"E":$"../../Vtxs/Vtx22",
-		"W":null	
-	},
-	$"../../Vtxs/Vtx24":{
-		"N":null,
-		"S":$"../../Vtxs/Vtx20",
-		"E":$"../../Vtxs/Vtx15",
-		"W":$"../../Vtxs/Vtx16"	
-	},
-	
-}
+# Lista de vértices conectados via neighbors corretamente
+@onready var listaCaminho := [
+	$"../../vertices/Inter/Vertice14",
+	$"../../vertices/Inter/Vertice2",
+	$"../../vertices/Inter/Vertice",
+	$"../../vertices/Inter/Vertice3",
+	$"../../vertices/Inter/Vertice4",
+	$"../../vertices/Inter/Vertice5",
+	$"../../vertices/Inter/Vertice7",
+	$"../../vertices/Inter/Vertice8",
+	$"../../vertices/Inter/Vertice9",
+	$"../../vertices/Inter/Vertice10",
+	$"../../vertices/Inter/Vertice11",
+	$"../../vertices/Inter/Vertice12",
+	$"../../vertices/Inter/Vertice13",
+	$"../../vertices/Inter/Vertice18",
+	$"../../vertices/Inter/Vertice13",
+	$"../../vertices/Inter/Vertice18",
+	$"../../vertices/Inter/Vertice15",
+	$"../../vertices/Inter/Vertice17",
+	$"../../vertices/Inter/Vertice18",
+	$"../../vertices/Inter/Vertice19",
+	$"../../vertices/Inter/Vertice20",
+	$"../../vertices/Inter/Vertice21",
+	$"../../vertices/Inter/Vertice22",
+	$"../../vertices/Inter/Vertice23",
+	$"../../vertices/Inter/Vertice6"
+]
 
-
-
-
-
-
-# Caminho real calculado
-@onready var caminho_atual: Array = []
-
-
-@onready var nodeAnterior = [null,null]
-@onready var nodeAtual = [null,null]
-@onready var destinoAtual = [null,null]
-
-@onready var distancia_total = 0
-
+var caminho_atual: Array = []
+var nodeAtual = null
 
 func _ready():
-	var vtxs_node = get_node("../../Vtxs")  # ajuste o caminho conforme sua estrutura
-	for child in vtxs_node.get_children():
-		if child.has_signal("player_entered_collision"):
-			child.connect("player_entered_collision", Callable(self, "colisao_vtx"))
-	
-	nodeAnterior[0] = $"../../Vtxs/Vtx14"
-	nodeAnterior[1] = sentidos.EAST
-	
-	nodeAtual[0] = $"../../Vtxs/Vtx14"
-	nodeAtual[1] = sentidos.EAST
-	destinoAtual[0] = $"../../Vtxs/Vtx6"
-	montar_caminho(nodeAtual,destinoAtual)
+	camera.position = Vector3(0, 1, 0)
+	camera.fov = 90
 
+	var start = listaCaminho[0]
 
-var verif = false
-func colisao_vtx(direcao,vtx):
-	alterar_node(true)
-	if verif:	
-		verif = false	
-		if vtx==nodeAtual[0]:
-			if caminho_atual.size()>1:
-				if direcao == caminho_atual[1][1]:
-					
-					montar_caminho(caminho_atual[1],destinoAtual)
-	if not verif:
-		if vtx==nodeAtual[0]:
-			var dir = nodeAtual[1]
-			var rev = dir
-			if dir==0:
-				rev=1
-			elif dir==1:
-				rev=0
-			elif dir==2:
-				rev=3
-			else:
-				rev = 2
-			if direcao ==rev:
-				verif = true
-				
-	
-	
+	# 🔥 Sorteia ponto e desativa os outros
+	randomize()
+	var pontos = [
+		get_node("../../Ponto"),
+		get_node("../../Ponto2"),
+		get_node("../../Ponto3")
+	]
+	var npcs = [
+		get_node("../../NPC"),
+		get_node("../../NPC2"),
+		get_node("../../NPC3")
+	]
 
-func definir_caminho(start):
-	return $"../../Vtxs/Vtx11"
+	var index = randi() % pontos.size()
+	var ponto_escolhido = pontos[index]
+	var goal = get_vertice_mais_proximo(ponto_escolhido.global_position)
 
+	# Desativa os pontos e NPCs não sorteados
+	for i in range(pontos.size()):
+		var ativo = (i == index)
+		pontos[i].monitoring = ativo
+		pontos[i].monitorable = ativo
+		pontos[i].visible = ativo
+		npcs[i].visible = ativo
 
-func dijkstra_basico(startN: Array, goalN: Array) -> Array:
-	var start = startN[0]
-	var goal = goalN[0]
-	
-	
-	
-	
-	
+	print("🎯 Ponto escolhido como destino: ", ponto_escolhido.name)
+
+	caminho_atual = dijkstra_basico(listaCaminho, start, goal)
+	montar_caminho(caminho_atual, start)
+
+func dijkstra_basico(vertices: Array, start: Node3D, goal: Node3D) -> Array:
 	var dist = {}
 	var prev = {}
-	var sentido = {}
 	var nao_visitados = []
 
-	for v in listaCaminho:
+	for v in vertices:
 		dist[v] = INF
 		prev[v] = null
-		sentido[v] = null
 		nao_visitados.append(v)
 
 	dist[start] = 0
-	sentido[start] = startN[1]
+
 	while nao_visitados.size() > 0:
 		nao_visitados.sort_custom(func(a, b): return dist[a] < dist[b])
 		var atual = nao_visitados.pop_front()
 
-		#if atual == goal:
-		#	break
-		
-		var vizinhos = [listaCaminho[atual]["N"],listaCaminho[atual]["S"],listaCaminho[atual]["E"],listaCaminho[atual]["W"]]
-		
-		
-		for vizinho in vizinhos:
+		if atual == goal:
+			break
+
+		for vizinho in atual.neighbors:
 			if vizinho == null or not dist.has(vizinho):
 				continue
 			var alt = dist[atual] + atual.global_position.distance_to(vizinho.global_position)
 			if alt < dist[vizinho]:
 				dist[vizinho] = alt
 				prev[vizinho] = atual
-				if listaCaminho[atual]["N"] == vizinho:
-					sentido[vizinho] = sentidos.NORTH
-				elif listaCaminho[atual]["S"] == vizinho:
-					sentido[vizinho] = sentidos.SOUTH
-				elif listaCaminho[atual]["E"] == vizinho:
-					sentido[vizinho] = sentidos.EAST
-				else:
-					sentido[vizinho] = sentidos.WEST
-		
 
 	var caminho = []
 	var u = goal
 	while u != null:
-		caminho.insert(0, [u,sentido[u]])
+		caminho.insert(0, u)
 		u = prev[u]
-		##############################
-		#print(caminho)
-	return [caminho,dist[goal]]
-	
 
+	return caminho
 
-func montar_caminho(start,goal):
-	
-	
-	var resposta = dijkstra_basico(start,goal)
-	caminho_atual = resposta[0]
-	distancia_total = resposta[1]
-	
+func montar_caminho(lista: Array, node):
 	line.clear_points()
 	mapa.line_clear()
+	var idx = lista.find(node)
+	if idx == -1:
+		idx = 0
+
+	var listaC = lista.slice(idx, lista.size())
+
 	var first = true
-	for i in caminho_atual:
-		var vertice = i[0]
-		line.add_point(Vector2(vertice.position.z * 3 + 60, -vertice.position.x * 3 + 68))
+	for vertice in listaC:
+		var point = Vector2(vertice.position.z * 3 + 60, -vertice.position.x * 3 + 70)
+		line.add_point(point)
 		mapa.create_line_point(vertice.position.z, vertice.position.x)
 		if first:
-			line.add_point(Vector2(vertice.position.z * 3 + 60, -vertice.position.x * 3 + 68))
-			mapa.create_line_point(vertice.position.z, vertice.position.x)
 			first = false
-	
-
 
 func _process(delta: float) -> void:
 	if target:
-		
-		img.position = Vector2((-3 * player.position.z + 40) , (3 * player.position.x + 30))
+		img.position = Vector2((-3 * player.position.z + 40) * zoom_factor, (3 * player.position.x + 30) * zoom_factor)
 		arrow.rotation = -player.global_rotation.y + 80.1
-		distanceLabel.text = "%d m" % (distancia_total + player.global_position.distance_to(caminho_atual[0][0].global_position))
-		mapa.distanceLabel.text = distanceLabel.text
-		if caminho_atual.size() > 0:
-			if line.get_point_count() > 0:
-				line.set_point_position(0, Vector2((player.position.z * 3 + 60) , (-player.position.x * 3 + 70)))
-			mapa.line_follow(player.position.z, player.position.x)
-			alterar_node(false)
 
-func alterar_node(alt):
-	var prox = get_mais_proximo()
-			
-	if prox[0] != nodeAtual[0]:
-		
-		if distancia_total> dijkstra_basico(prox,destinoAtual)[1] || alt:
-		
-			nodeAnterior[0] = nodeAtual[0]
-			nodeAnterior[1] = nodeAtual[1]
-			if listaCaminho[nodeAtual[0]]["N"] == prox[0]:
-				prox[1] = sentidos.NORTH
-			elif listaCaminho[nodeAtual[0]]["S"] == prox[0]:
-				prox[1] = sentidos.SOUTH
-			elif listaCaminho[nodeAtual[0]]["E"] == prox[0]:
-				prox[1] = sentidos.EAST
-			elif listaCaminho[nodeAtual[0]]["W"] == prox[0]:
-				prox[1] = sentidos.WEST
-			nodeAtual[0] = prox[0]
-			nodeAtual[1] = prox[1]
-			montar_caminho(prox,destinoAtual)
+		if caminho_atual.size() > 0:
+			line.set_point_position(0, Vector2((player.position.z * 3 + 60) * zoom_factor, (-player.position.x * 3 + 70) * zoom_factor))
+			mapa.line_follow(player.position.z, player.position.x)
+
+			var prox = get_mais_proximo()
+			if prox != nodeAtual:
+				nodeAtual = prox
+				montar_caminho(caminho_atual, prox)
 
 func get_mais_proximo():
-	
 	var menor_dist = INF
-	var prox: Array = [null,null]
-	
-	for ponto in listaCaminho:
-		
+	var prox: Node3D = null
+	for ponto in caminho_atual:
 		var distancia = player.global_position.distance_to(ponto.global_position)
 		if distancia < menor_dist:
 			menor_dist = distancia
-			prox[0] = ponto
-			
+			prox = ponto
 	return prox
-	
+
+func get_vertice_mais_proximo(pos: Vector3) -> Node3D:
+	var menor_dist = INF
+	var mais_proximo = null
+	for v in listaCaminho:
+		var d = v.global_position.distance_to(pos)
+		if d < menor_dist:
+			menor_dist = d
+			mais_proximo = v
+	return mais_proximo
--- npc.gd
+++ npc.gd
@@ -6,34 +6,58 @@ enum State { IDLE, APPROACH_PLAYER, DISAPPEAR, REAPPEAR, WALK_AWAY }
 
 var current_state = State.IDLE
 var speed = 2.0
+
 @export var playerP: NodePath
-var target_position = Vector3.ZERO
-var walk_away_position = Vector3(1000, 0, 10) # lugar onde ele vai depois de reaparecer
 var player = null
+var target_position = Vector3.ZERO
+var walk_away_position = Vector3(1000, 0, 10)
+
 @onready var animation = $AnimationPlayer
 
 func _ready():
-	player = get_node(playerP) # ajuste o caminho até o Player
+	# ✅ Garante que player é inicializado corretamente
+	if player == null:
+		if playerP != null and has_node(playerP):
+			player = get_node(playerP)
+		elif has_node("../carro"):
+			player = get_node("../carro")  # fallback
+		else:
+			print("🚫 [NPC] Caminho para o jogador não encontrado.")
+
 	animation.play("idleN")
 
+	# Conecta dinamicamente ao ponto correto com base no nome
+	var ponto_name = ""
+	match name:
+		"NPC":
+			ponto_name = "../Ponto"
+		"NPC2":
+			ponto_name = "../Ponto2"
+		"NPC3":
+			ponto_name = "../Ponto3"
+
+	if ponto_name != "" and has_node(ponto_name):
+		get_node(ponto_name).connect("body_entered", Callable(self, "_on_ponto_body_entered"))
+
+	# Conecta ao destino fixo
+	if has_node("../destino"):
+		get_node("../destino").connect("body_entered", Callable(self, "_on_destino_body_entered"))
+
+
 func _physics_process(delta):
-	
 	match current_state:
 		State.IDLE:
 			animation.play("idleN")
-			# espera alguma condição externa
 		State.APPROACH_PLAYER:
 			move_towards(player.global_transform.origin, delta)
 			animation.play("walkN")
 			if is_close_to(player.global_transform.origin):
-				
-				player.set_frozen(false)
+				if player:
+					player.set_frozen(false)
 				current_state = State.DISAPPEAR
 		State.DISAPPEAR:
 			visible = false
-			velocity = Vector3.ZERO  # Garantir que o NPC pare de se mover
-			
-			
+			velocity = Vector3.ZERO
 		State.REAPPEAR:
 			global_transform.origin = player.global_transform.origin + Vector3(3, 0, 0)
 			visible = true
@@ -42,39 +66,38 @@ func _physics_process(delta):
 			move_towards(walk_away_position, delta)
 			animation.play("walkN")
 			if is_close_to(walk_away_position):
-				
 				visible = false
 				current_state = State.IDLE
+
 
 func move_towards(target: Vector3, delta: float):
 	var direction = (target - global_transform.origin).normalized()
 	velocity = direction * speed
-	
 	var look_target = global_transform.origin - direction
 	look_at(look_target, Vector3.UP)
 	move_and_slide()
 
+
 func is_close_to(target: Vector3) -> bool:
 	return global_transform.origin.distance_to(target) < 3.0
-	
 
 
 func _on_ponto_body_entered(body: Node3D) -> void:
-	if body.name == "carro":
-		
-		if not passageiro:
+	if body.name == "carro" and not passageiro:
+		if player:
 			player.set_frozen(true)
-			await get_tree().create_timer(1.0).timeout
-			current_state = State.APPROACH_PLAYER
-			passageiro = true
+		await get_tree().create_timer(1.0).timeout
+		current_state = State.APPROACH_PLAYER
+		passageiro = true
 
 
 func _on_destino_body_entered(body: Node3D) -> void:
-	if body.name == "carro":
-		if passageiro:
+	if body.name == "carro" and passageiro:
+		if player:
 			player.set_frozen(true)
-			await get_tree().create_timer(1.0).timeout
-			current_state = State.REAPPEAR
-			passageiro = false
-			await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(1.0).timeout
+		current_state = State.REAPPEAR
+		passageiro = false
+		await get_tree().create_timer(1.0).timeout
+		if player:
 			player.set_frozen(false)
